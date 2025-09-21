ea2341e172e87414c45b4d95aa69a9ca9c350c1b
Leonardo Carvalho <leonardokarvalho@gmail.com>
2025-09-21 20:39:10 -0300

    Update User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,6 +49,7 @@ class User extends Authenticatable implements MustVerifyEmail
         return [
             'email_verified_at' => 'datetime',
             'password'          => 'hashed',
+             'role' => \App\Enums\UserRole::class
         ];
     }
 }
