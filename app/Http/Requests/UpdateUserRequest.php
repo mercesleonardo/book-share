22c64fe33d5a7e2cb564d61ecb8c6863ad090072
Leonardo Carvalho <leonardokarvalho@gmail.com>
2025-09-21 20:46:43 -0300

    Add UpdateUserRequest for user update validation

---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\UserRole;
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name'  => ['required', 'string', 'max:255'],
+            'email' => [
+                'required',
+                'string',
+                'email',
+                'max:255',
+                Rule::unique(User::class)->ignore($this->user()->id),
+            ],
+            'profile_photo' => ['nullable', 'image', 'mimes:jpg,jpeg,png', 'max:2000'],
+            'description'   => ['nullable', 'string', 'max:1000'],
+            'password'      => ['nullable', 'string', 'min:8'],
+            'role'          => ['required', 'string',   Rule::in(array_map(fn($case) => $case->value, UserRole::cases()))],
+        ];
+    }
+}
