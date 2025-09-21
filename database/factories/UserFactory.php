41c4f10f3a879b0cb6cb80b17d0bb35465c0eeea
Leonardo Carvalho <leonardokarvalho@gmail.com>
2025-09-21 20:39:58 -0300

    Improve UserFactory to support new user attributes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Enums\UserRole;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -28,7 +29,7 @@ class UserFactory extends Factory
             'email'             => fake()->unique()->safeEmail(),
             'profile_photo'     => fake()->imageUrl(),
             'description'       => fake()->text(),
-            'role'              => 'user',
+            'role'              => UserRole::USER,
             'email_verified_at' => now(),
             'password'          => static::$password ??= Hash::make('password'),
             'remember_token'    => Str::random(10),
